29e4aabd87921e789d423be08b8ee9e3c342ec59
Bulliwf <bulliwft@gmail.com>
2023-04-24 00:19:04 -0400

    another commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { AmplifySignOut } from "@aws-amplify/ui-react/styles.css";
-import { API, Storage } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { API, Storage, Auth } from "aws-amplify";
 import {Button, Flex, Heading, Image, Text, TextField, View, withAuthenticator} from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {createNote as createNoteMutation, deleteNote as deleteNoteMutation} from "./graphql/mutations";
@@ -11,10 +11,17 @@ const initialFormState = { name: '', description: ''}
 function App() {
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
-
   useEffect(() => {
     fetchNotes();
   }, []);
+    
+    const App = ({signOut}) => {
+        const [notes, setNotes] = useState([]);
+        useEffect(() => {
+        fetchNotes();
+      }, []);
+
+}
 
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
@@ -45,7 +52,8 @@ function App() {
   async function deleteNote({ id }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
-    await Storage.remove(name);
+    await Storage.remove(notes.name);
+    await Storage.remove(notes.name);
     await API.graphql({query: deleteNoteMutation, variables: { input: { id }}});
   }
   async function onChange(e) {
@@ -58,16 +66,16 @@ function App() {
 
   return (
     <div className="App">
-      <h1 class="bannerText">Daily Fitness Tracker</h1>
-      <div id="signout"><AmplifySignOut/></div>
+      
+      <h1 className="bannerText">WolfTech Planner</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
+        placeholder="Note Name"
         value={formData.name}
       />
       <input
         onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
+        placeholder="Description"
         value={formData.description}
       />
       <input
@@ -78,7 +86,7 @@ function App() {
         {
           notes.map(note => (
             <div key={note.id || note.name}>
-              <p>==================================================================<br/></p>
+              <p>_______________________________________________________________<br/></p>
               <h2 id="noteName">{note.name}</h2>
               <p>{note.description}</p>
               {
